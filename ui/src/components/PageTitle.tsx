603c337b24ac0695a4a5401ca7169af5d8b17a25
Krzysztof Nizioł <kniziol@gmail.com>
2026-06-26 14:59:57 +0200

    Vitest unit tests of the <PageTitle> component

--- ui/src/components/PageTitle.tsx
+++ ui/src/components/PageTitle.tsx
@@ -1,5 +1,6 @@
 import { APP_NAME } from "../config/app.ts";
 import { useMemo } from "react";
+import { TITLE_SEPARATOR } from "../config/pageTitle.ts";
 
 type Props = {
   title: string
@@ -7,11 +8,13 @@ type Props = {
 
 export default function PageTitle({title}: Props) {
   const fullTitle = useMemo(() => {
-    if (!title) {
+    const clearTitle = title.trim();
+
+    if (!clearTitle) {
       return APP_NAME;
     }
 
-    return title + " · " + APP_NAME;
+    return clearTitle + TITLE_SEPARATOR + APP_NAME;
   }, [title]);
 
   return (
